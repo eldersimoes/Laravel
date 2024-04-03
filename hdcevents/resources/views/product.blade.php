669a1612ce56c4d744db52cf28e82b41475b75a1
eldersimoes <eldersimoes.85@gmail.com>
2024-04-03 00:12:32 -0300

    Config yield , extends , section
    configuração de layout usando extend, yield e section

--- hdcevents/resources/views/product.blade.php
+++ hdcevents/resources/views/product.blade.php
@@ -1,2 +1,10 @@
-<h1>Esta será a pagina de produtos</h1>
-<a href="/">Voltar para Home</a>
+@extends('layouts.main')
+
+@section('title','Produtos')
+
+@section('content')    
+
+    <h1>Esta será a pagina de produtos</h1>
+    <a href="/">Voltar para Home</a>
+
+@endsection
--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -1,56 +1,46 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title','HDC Events')
 
-         <link rel="stylesheet" href="/css/styless.css">  
-         <script src="/js/scripts.js"></script>
-    </head>
-    <body>
-        <h1>Algum titulo</h1>
+@section('content')
+    
 
-        <img src="/img/banner.jpg" alt="Banner">
+<h1>Algum titulo</h1>
 
-        @if(10>15)
+<img src="/img/banner.jpg" alt="Banner">
 
-            <p>a condição é true</p>
+    @if(10>15)
+        <p>a condição é true</p>
+    @endif
+
+    <p> {{$nome}} </p>
+
+    @if($nome == "Pedro")
+        <p>o nome é: {{$nome}};</p>
+        <p>Ele tem: {{$idade}} anos;</p>
+        <p>Sua profissão é: {{$profissao}}.</p>
+    @else
+        <p>o nome não é Pedro, é {{$nome}}</p>
+    @endif
+
+    @for($i = 0; $i <  count($arr); $i++)
+        <p> {{$arr[$i]}} - {{$i}} </p>
         
+        @if($i==2)
+            <p>O i agora é 2</p>
         @endif
+    @endfor
 
-        <p> {{$nome}} </p>
-
-        @if($nome == "Pedro")
-            <p>o nome é: {{$nome}};</p>
-            <p>Ele tem: {{$idade}} anos;</p>
-            <p>Sua profissão é: {{$profissao}}.</p>
-        @else
-            <p>o nome não é Pedro, é {{$nome}}</p>
-        @endif
-
-        @for($i = 0; $i <  count($arr); $i++)
-            <p> {{$arr[$i]}} - {{$i}} </p>
-            
-            @if($i==2)
-                <p>O i agora é 2</p>
-            @endif
-
-        @endfor
-
-        @php
-            $phpname = "João";
-            echo $phpname;    
-        @endphp
-            
-        <!--Comentario html aparece no inspecionar elemento compromete a segurança-->
-        {{--comentario do blade nao aparece no inspecionar elemento mais seguro--}}
-
-        @foreach ($nomes as $j)
-            <p>{{$loop->index}} - {{$j}}</p>
-        @endforeach
+    @php
+        $phpname = "João";
+        echo $phpname;    
+    @endphp
         
+    <!--Comentario html aparece no inspecionar elemento compromete a segurança-->
+    {{--comentario do blade nao aparece no inspecionar elemento mais seguro--}}
 
-    </body>
-</html>
+    @foreach ($nomes as $j)
+        <p>{{$loop->index}} - {{$j}}</p>
+    @endforeach
+
+@endsection    
